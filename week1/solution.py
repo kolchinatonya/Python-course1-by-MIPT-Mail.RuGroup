875a564254abf82f9bb1fffc2dc5002f55bb3c7e
Antonina Kolchina <38192817+kolchinatonya@users.noreply.github.com>
2020-09-15 14:00:00 +0300

    Add files via upload

---
+++ week1/solution.py
@@ -0,0 +1,10 @@
+from math import sqrt
+import sys 
+a = int(sys.argv[1]) 
+b = int(sys.argv[2]) 
+c = int(sys.argv[3])
+D = b ** 2 - 4 * a * c
+x1 = int((-b + sqrt(D))/(2 * a))
+x2 = int((-b - sqrt(D))/(2 * a))
+print(x1, x2, sep = '\n')
+
---
+++ week2/to_json.py
@@ -0,0 +1,15 @@
+import functools
+import json
+
+def to_json(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        result = json.dumps(func(*args, **kwargs))
+        return result
+    return wrapped
+
+@to_json
+def get_data():
+    return {'data': 42}
+  
+get_data()
